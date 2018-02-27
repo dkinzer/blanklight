60c2a13fb2d3c528fb64e711e439e9bbf3e6210e
David Kinzer <dtkinzer@gmail.com>
2018-02-27 12:35:42 -0500

    Skip ingest for now since that's not an oob feature.

--- bin/app-start.rb
+++ bin/app-start.rb
@@ -20,8 +20,8 @@ exec("rails s -p 3000 -b '0.0.0.0'") if fork == nil
 
 # Next, provision with test data.
 # (If we do this first it works, but site will be blank until rails app loads).
-`rake fortytu:solr:load_fixtures`
-`rake ingest`
+#`rake fortytu:solr:load_fixtures`
+#`rake ingest`
 
 # Wait for rails server to shutdown before stopping the process.
 Process.wait
